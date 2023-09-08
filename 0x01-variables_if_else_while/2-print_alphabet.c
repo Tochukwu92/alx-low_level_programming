2a5465aa0c51595a88ea920e124659a4dfcdd6f8
Tochukwu92 <ilonubatochukwu@gmail.com>
2023-09-08 07:47:35 -0700

    variable and if, else task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Execution starts here
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	char ch
+		;
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
+

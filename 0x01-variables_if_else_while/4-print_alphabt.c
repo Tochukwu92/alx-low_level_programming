1d109fb6b38589d6ed591531f108db495354e336
Tochukwu92 <ilonubatochukwu@gmail.com>
2023-09-08 09:34:13 -0700

    variable and if, else task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Execution starts here
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}

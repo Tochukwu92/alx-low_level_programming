352f4230a50ef4035236281da9eb20d945e272dc
Tochukwu92 <ilonubatochukwu@gmail.com>
2023-09-08 11:53:54 -0700

    variable and if, else task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-	int a;
+	int q;
 
-	for (a = 0; a < 0; a++)
+	for (q = 0; q < 10; q++)
 	{
-		putchar(a + '0');
-		if (a < 9)
+		putchar(q + '0');
+		if (q < 9)
 		{
 			putchar(',');
 			putchar(' ');

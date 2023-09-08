99e253ca8e8601191e89e29fdc6f10d82e3eb769
Tochukwu92 <ilonubatochukwu@gmail.com>
2023-09-08 10:16:52 -0700

    variable and if, else task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Execution starts here
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int v;
+
+	for (v = 0; v < 10; v++)
+		putchar(v);
+	putchar('\n');
+	return (0);
+}

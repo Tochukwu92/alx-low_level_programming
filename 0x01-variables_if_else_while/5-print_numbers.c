655a6311cffdc5951cb80758714537b20ab5409b
Tochukwu92 <ilonubatochukwu@gmail.com>
2023-09-08 09:50:49 -0700

    variable and if, else task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Execution starts here
+ *
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int r;
+
+	for (r = 0; r < 10; r++)
+		printf("%d\n", r);
+	return (0);
+}

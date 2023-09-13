af7fa12c3706a7792f9628a3590a71e6e6ea264d
Tochukwu92 <ilonubatochukwu@gmail.com>
2023-09-13 05:04:26 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints all natural numbers below 1024
+ * Return: returns 0 when successful
+ */
+int main(void)
+{
+	int a, sum = 0;
+
+	for (a = 0; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+		{
+			sum += a;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints the sum of the even-valued terms of a fibonacci sequence
+ * Return: always return zero when successful
+ */
+int main(void)
+{
+	int x;
+	unsigned long int y, z, n, sum;
+
+	y = 1;
+	z = 2;
+	sum = 0;
+	for (x = 1; x <= 33; ++x)
+	{
+		if (y < 4000000 && (y % 2) == 0)
+		{
+			sum += y;
+		}
+		n = y + z;
+		y = z;
+		z = n
+	}
+	printf("%lu\n", sum);
+	return (0);
+}

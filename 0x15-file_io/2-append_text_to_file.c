66a948a21dae7711e19ebcb4d8349976652a7e8e
Tochukwu92 <ilonubatochukwu@gmail.com>
2023-10-30 12:54:54 -0700

    Alx-Holberton c-file I/O task

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 	num = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	w = write(num, text_content, len);
 	if (num == -1 || w == -1)
 		return (-1);
 	close(num);

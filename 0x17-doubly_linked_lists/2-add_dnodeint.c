72050fe27fe96b5eda683da940d87bb06b81f0da
Tochukwu92 <ilonubatochukwu@gmail.com>
2023-12-08 05:23:24 -0800

    doubly linked_list task

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	temp = *head;
 	new_node->n = n;
-	
+
 	if (temp != NULL)
 	{
 		temp->prev = new_node;
@@ -29,6 +29,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		new_node->prev = NULL;
 		temp = new_node;
 	}
+
 	*head = new_node;
 	return (new_node);
 }

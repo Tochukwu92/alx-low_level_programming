82fbef2a3bf54ca375cea259a268c5b3686dd307
Tochukwu92 <ilonubatochukwu@gmail.com>
2023-12-12 11:04:39 -0800

    nam

---
+++ 0x18-dynamic_libraries/func.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+
+/**
+ * add - add two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: addition
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - sub two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: substitution
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * div - divide two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: division
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mul - add two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * mod - remainder of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: multiplication
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
